229df4670112f4067a8211d98f761128e1236dbe
Yazz <itsjustmaitai@gmail.com>
2025-06-24 00:28:58 +0300

    Updated guest_controller

--- server/controllers/guest_controller.py
+++ server/controllers/guest_controller.py
@@ -1,10 +1,26 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from server.models.guest import Guest
 from server.app import db
 
 guest_bp = Blueprint('guests', __name__)
 
+# GET all guests
 @guest_bp.route('/guests', methods=['GET'])
 def get_guests():
     guests = Guest.query.all()
     return jsonify([guest.to_dict() for guest in guests]), 200
+
+# ✅ POST a new guest with validation
+@guest_bp.route('/guests', methods=['POST'])
+def create_guest():
+    data = request.get_json()
+
+    if not data.get('name') or not data.get('occupation'):
+        return jsonify({'error': 'Missing guest name or occupation'}), 400
+
+    new_guest = Guest(name=data['name'], occupation=data['occupation'])
+    db.session.add(new_guest)
+    db.session.commit()
+
+    return jsonify(new_guest.to_dict()), 201
+
